d7eecb3d766526a4a2e2abd0e7682323f92b1bd0
Gabriel Dias Faria <gabrieldiasfaria@gmail.com>
2020-01-15 10:16:45 +0000

    Socket.Io

--- 1st_release/server.js
+++ 1st_release/server.js
@@ -1,13 +1,25 @@
-const express = require("express");
-const http = require("http");
+//const express = require("express");
+//const http = require("http");
 
-//import express from 'express'
-//import http from 'http'
+// ctrl+c => desliga servidor
+// npx nodemon => nodemon deixa o server com hotReload
+// npx serve => servidor temporário para rodar projeto
+
+import express from 'express'
+import http from 'http'
+import createGame from './public/createGame.js'
 
 const app = express()
 const server = http.createServer(app)
 
 app.use(express.static('public'))
+
+const game = createGame()
+
+game.addPlayer({ playerId: "player1", playerX: 1, playerY: 2, playerColor: "black" });
+game.addPlayer({ playerId: "player2", playerX: 1, playerY: 4, playerColor: "black"  });
+
+game.addFruit({ fruitId: "fruit1", fruitX: 4, fruitY: 3, fruitColor: 'green' });
 
 server.listen(3000, () => {
     console.log("Servidor iniciado, porta: 3000")
