b88f0d1677acdafc8f45edafdd29a662e85d12f5
Gabriel Dias Faria <36157517+GabrielDiasFaria@users.noreply.github.com>
2020-01-20 23:26:37 +0000

    Update Player.java

--- Projects/Pong/src/Pong/Player.java
+++ Projects/Pong/src/Pong/Player.java
@@ -4,14 +4,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Player {
+	
+	public boolean right,left,up,down;
+	
+	public int x,y,width,height;
+	
+	Player(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
 
 	public void tick(){
+		if(right) {
+			x++;
+		} else if(left) {
+			x--;
+		}
+		
+		if(x+width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		} else if(x < 0) {
+			x = 0;
+		}
 		
 	}
 	
 	public void render(Graphics g){
 		g.setColor(Color.blue);
-		g.fillRect(200, 110, 40, 10);
+		g.fillRect(x, y, width, height);
 	}
 	
 }
