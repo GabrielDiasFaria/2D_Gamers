5b51432ef43dad75c17745bb220d144dd7e89446
Gabriel Dias Faria <36157517+GabrielDiasFaria@users.noreply.github.com>
2020-01-20 23:27:01 +0000

    Create Enemy.java

---
+++ Projects/Pong/src/Pong/Enemy.java
@@ -0,0 +1,28 @@
+package Pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Enemy {
+	
+	public double x,y;
+	public int width,height;
+	
+	public Enemy(double x,double y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
+
+	public void tick() {
+		x+= (Game.ball.x - x - 6) * 0.4;
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect((int)x, (int)y, width, height);
+	}
+	
+}
